f80bdf9542803f376bfd734d9d80bf8d11fcba18
damedelion <ivan.shishkov7@yandex.ru>
2025-03-29 00:24:18 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # kv_tarantool
+
+key-value хранилище с хранением данных в Tarantool
+
+API:
+- POST /kv body: {key: "test", "value": {SOME ARBITRARY JSON}} 
+- PUT kv/{id} body: {"value": {SOME ARBITRARY JSON}}
+- GET kv/{id} 
+- DELETE kv/{id}
+
+Ответы:
+- POST  возвращает 409 если ключ уже существует, 
+- POST, PUT возвращают 400 если боди некорректное
+- PUT, GET, DELETE возвращает 404 если такого ключа нет
+
+Запуск:
+```
+docker compose build
+docker compose up
+```
+
+Завершение:
+```
+docker compose down
+```
--- internal/kv/delivery/http/http.go
+++ internal/kv/delivery/http/http.go
@@ -7,13 +7,18 @@ import (
 	"strconv"
 
 	"github.com/damedelion/kv_tarantool/internal/dto"
+	"github.com/damedelion/kv_tarantool/internal/interrors"
 	"github.com/damedelion/kv_tarantool/internal/kv"
 	"github.com/damedelion/kv_tarantool/internal/models"
+	"github.com/damedelion/kv_tarantool/internal/utils"
+	"github.com/damedelion/kv_tarantool/pkg/logger"
 	"github.com/gorilla/mux"
+	"go.uber.org/zap"
 )
 
 type delivery struct {
 	usecase kv.Usecase
+	logger  logger.Logger
 }
 
 func New(usecase kv.Usecase) kv.Delivery {
@@ -21,30 +26,38 @@ func New(usecase kv.Usecase) kv.Delivery {
 }
 
 func (d *delivery) Get(w http.ResponseWriter, r *http.Request) {
+	requestID := r.Context().Value(utils.RequestIDKey{})
 	vars := mux.Vars(r)
 	key, _ := strconv.Atoi(vars["key"])
 
 	item, err := d.usecase.Get(key)
 	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
+		switch err.(type) {
+		case *interrors.KeyNotFound:
+			w.WriteHeader(http.StatusNotFound)
+			d.logger.Error("key not found", requestID, zap.String("msg", err.Error()))
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+			d.logger.Error("unknown error", requestID, zap.String("msg", err.Error()))
+		}
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(item)
 	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
+		d.logger.Error("unknown error", requestID, zap.String("msg", err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
 
 func (d *delivery) Post(w http.ResponseWriter, r *http.Request) {
+	requestID := r.Context().Value(utils.RequestIDKey{})
 	item := &models.Item{}
 
 	if err := json.NewDecoder(r.Body).Decode(item); err != nil {
-		fmt.Println(err)
+		d.logger.Error("bad request", requestID, zap.String("msg", err.Error()))
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -54,28 +67,35 @@ func (d *delivery) Post(w http.ResponseWriter, r *http.Request) {
 
 	err := d.usecase.Post(dtoItem)
 	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
+		switch err.(type) {
+		case *interrors.DuplicateKey:
+			w.WriteHeader(http.StatusConflict)
+			d.logger.Error("duplicate key", requestID, zap.String("msg", err.Error()))
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+			d.logger.Error("unknown error", requestID, zap.String("msg", err.Error()))
+		}
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(item)
 	if err != nil {
-		fmt.Println("failed to encode body")
-		w.WriteHeader(http.StatusBadRequest)
+		d.logger.Error("unknown error", requestID, zap.String("msg", err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
 
 func (d *delivery) Put(w http.ResponseWriter, r *http.Request) {
+	requestID := r.Context().Value(utils.RequestIDKey{})
 	vars := mux.Vars(r)
 	key, _ := strconv.Atoi(vars["key"])
 
 	item := &models.Item{}
 
 	if err := json.NewDecoder(r.Body).Decode(item); err != nil {
-		fmt.Println(err)
+		d.logger.Error("unknown error", requestID, zap.String("msg", err.Error()))
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -84,28 +104,41 @@ func (d *delivery) Put(w http.ResponseWriter, r *http.Request) {
 
 	err := d.usecase.Put(dtoItem)
 	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
+		switch err.(type) {
+		case *interrors.KeyNotFound:
+			w.WriteHeader(http.StatusNotFound)
+			d.logger.Error("key not found", requestID, zap.String("msg", err.Error()))
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+			d.logger.Error("unknown error", requestID, zap.String("msg", err.Error()))
+		}
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(item)
 	if err != nil {
-		fmt.Println("failed to encode body")
-		w.WriteHeader(http.StatusBadRequest)
+		d.logger.Error("unknown error", requestID, zap.String("msg", err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
 
 func (d *delivery) Delete(w http.ResponseWriter, r *http.Request) {
+	requestID := r.Context().Value(utils.RequestIDKey{})
 	vars := mux.Vars(r)
 	key, _ := strconv.Atoi(vars["key"])
 
 	item, err := d.usecase.Delete(key)
 	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
+		switch err.(type) {
+		case *interrors.KeyNotFound:
+			w.WriteHeader(http.StatusNotFound)
+			d.logger.Error("key not found", requestID, zap.String("msg", err.Error()))
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+			d.logger.Error("unknown error", requestID, zap.String("msg", err.Error()))
+		}
 		return
 	}
 
